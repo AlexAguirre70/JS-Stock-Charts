4d234cff0d1876c4c9f36e8df9cbb4d783c1bea1
Alex Aguirre <teamaguirre@gmail.com>
2022-07-14 16:36:10 -0700

    DOM manipulation via JS to display charts

--- public/index.js
+++ public/index.js
@@ -1,50 +1,101 @@
-import { Chart } from "chart.js";
+function getColor(stock){
+    if(stock === "GME"){
+        return 'red'
+    }
+    if(stock === "MSFT"){
+        return 'blue'
+    }
+    if(stock === "DIS"){
+        return 'green'
+    }
+    if(stock === "BNTX"){
+        return 'yellow'
+    }
+}
 
-
-
-function main() {
-
+async function main() {
+    // Create the Canvas Variables
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
+    //get stock values via api
+    const response = await fetch('https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1day&apikey=b18ff0b8dcb44a8ba373b1c2938aaafb')   
+    const result = await response.json()
+    const { GME, MSFT, DIS, BNTX } = result;
+    const stocks = [GME, MSFT, DIS, BNTX];
+    stocks.forEach( stock => stock.values.reverse())
 
- //let response = await fetch('https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1day&apikey=b18ff0b8dcb44a8ba373b1c2938aaafb');
- // let results = response.json;
-    
-    const {GME,MSFT,DIS,BNTX}= mockData;
-    const stocks = [GME,MSFT,DIS,BNTX];
-    stocks.forEach(stock => stock.values.reverse())
-   
+    // Time Chart
+    new Chart(timeChartCanvas.getContext('2d'), {
+        type: 'line',
+        data: {
+            labels: stocks[0].values.map(value => value.datetime),
+            datasets: stocks.map(stock => ({
+                label: stock.meta.symbol,
+                backgroundColor: getColor(stock.meta.symbol),
+                borderColor: getColor(stock.meta.symbol),
+                data: stock.values.map(value => parseFloat(value.high))
+            }))
+        }
+    });
 
-//  Create base chart
-new Chart (timeChartCanvas.getContext('2d'), {
-    type: 'line',
-    data: {
-        labels: stocks[0].values.map(value => value.datetime),
-        datasets: stocks.map(stock => ({
-            label: stock.meta.symbol,
-            backgroundColor: getColor(stock.meta.symbol),
-            borderColor: getColor(stock.meta.symbol),
-            data: stock.values.map(value => parseFloat(value.high))
-        }))
-    }
-});
+    // High Chart
+    new Chart(highestPriceChartCanvas.getContext('2d'), {
+        type: 'bar',
+        data: {
+            labels: stocks.map(stock => stock.meta.symbol),
+            datasets: [{
+                label: 'Highest',
+                backgroundColor: stocks.map(stock => (
+                    getColor(stock.meta.symbol)
+                )),
+                borderColor: stocks.map(stock => (
+                    getColor(stock.meta.symbol)
+                )),
+                data: stocks.map(stock => (
+                    findHighest(stock.values)
+                ))
+            }]
+        }
+    });
+
+    // Average Chart
+    new Chart(averagePriceChartCanvas.getContext('2d'), {
+        type: 'pie',
+        data: {
+            labels: stocks.map(stock => stock.meta.symbol),
+            datasets: [{
+                label: 'Average',
+                backgroundColor: stocks.map(stock => (
+                    getColor(stock.meta.symbol)
+                )),
+                borderColor: stocks.map(stock => (
+                    getColor(stock.meta.symbol)
+                )),
+                data: stocks.map(stock => (
+                    calculateAverage(stock.values)
+                ))
+            }]
+        }
+    });
 }
 
-function getColor(stock){
-    if(stock === "GME"){
-        return 'rgba(61, 161, 61, 0.7)'
-    }
-    if(stock === "MSFT"){
-        return 'rgba(209, 4, 25, 0.7)'
-    }
-    if(stock === "DIS"){
-        return 'rgba(18, 4, 209, 0.7)'
-    }
-    if(stock === "BNTX"){
-        return 'rgba(166, 43, 158, 0.7)'
-    }
+function findHighest(values) {
+    let highest = 0;
+    values.forEach(value => {
+        if (parseFloat(value.high) > highest) {
+            highest = value.high
+        }
+    })
+    return highest
 }
 
+function calculateAverage(values) {
+    let total = 0;
+    values.forEach(value => {
+        total += parseFloat(value.high)
+    })
+    return total / values.length
+}
 
 main()
